0d8fcdea99824c9e5bf0355c3a1e439fe3a118e6
Junior chaves <juniorchaves0010@gmail.com>
2024-02-14 16:03:48 -0300

    bug terminado

--- js/decodificador.js
+++ js/decodificador.js
@@ -15,10 +15,13 @@ function criptografarText() {
     // chama a função criptografar, e colocar o valor criptografado no textarea.
     let input = document.querySelector("#adicionar_texto").value;
 
+    let textarea = document.querySelector("#decodificador_texto");
+
     // Verifica se o input está vazio
     if (input === "") {
         restalrarTags()
         removerTag("copiar", "#copia")
+        textarea.value = ""
         alert("Por favor, insira um texto para criptografar.");
         return; // Retorna sem fazer mais nada se o input estiver vazio
     }
@@ -27,6 +30,7 @@ function criptografarText() {
     if (/[À-ÖØ-öø-ÿA-Z]/.test(input)) {
         removerTag("copiar", "#copia")
         restalrarTags()
+        textarea.value = ""
         alert("O texto não pode conter letras maiúsculas ou com acento.");
         return; // Retorna sem fazer mais nada se o texto contiver letras maiúsculas ou com acento
     }
@@ -34,12 +38,13 @@ function criptografarText() {
     if (!/[aeiou]/i.test(input)) {
         restalrarTags()
         removerTag("copiar", "#copia")
+        textarea.value = ""
         alert("é presciso de pelo menos uma vogal para ser criptografado")
         return;
     }
 
 
-    let textarea = document.querySelector("#decodificador_texto");
+    
     input = criptografar(input);
     textarea.value = input;
     removerTag("subtitulo", "#subtitulo");
@@ -68,6 +73,8 @@ function descriptografarTexto() {
     if (input === "") {
         alert("Por favor, insira um texto criptografado para ser descriptografado.");
         restalrarTags()
+        textarea.value = ""
+        removerTag("copiar", "#copia")
     }
 
     let textarea = document.querySelector("#decodificador_texto");
@@ -79,6 +86,8 @@ function descriptografarTexto() {
     document.querySelector("#adicionar_texto").value = ""
     return input;
 }
+
+
 
 function removerTag(name, tag) {
     // retira as tags para ter uma melhor visão e colocara ou deixara as tags nescessarias.
@@ -93,11 +102,13 @@ function restalrarTags() {
         img.style.display = "flex"
         let subtitulo = document.querySelector("#subtitulo")
         subtitulo.style.display = "flex"
+
     }
     else {
         removerTag("subtitulo", "#subtitulo")
         removerTag("imagem", "#img_garoto")
     }
+    
 }
 // Adiciona um listener para o evento 'resize' que chama a função de ajuste de tags
 window.addEventListener('resize', restalrarTags);
@@ -119,4 +130,3 @@ function copiaTexto() {
     let textarea = document.getElementById("decodificador_texto");
     navigator.clipboard.writeText(textarea.value)
 }
-
