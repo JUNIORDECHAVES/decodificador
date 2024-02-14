bf72bc7afe08e30c27c23710ef34d4d4715fb7b8
Junior chaves <juniorchaves0010@gmail.com>
2024-02-14 15:40:22 -0300

    bugs resolvidos e ajustes concluidos

--- js/decodificador.js
+++ js/decodificador.js
@@ -1,25 +1,51 @@
 
 
-function criptografar (texto) {
+function criptografar(texto) {
     //criptografar as vogais
     texto = texto.replace(/e/g, "enter");
     texto = texto.replace(/i/g, "imes");
     texto = texto.replace(/a/g, "ai");
     texto = texto.replace(/o/g, "ober");
     texto = texto.replace(/u/g, "ufat");
-    
+
     return texto;
 }
 
 function criptografarText() {
     // chama a função criptografar, e colocar o valor criptografado no textarea.
     let input = document.querySelector("#adicionar_texto").value;
+
+    // Verifica se o input está vazio
+    if (input === "") {
+        restalrarTags()
+        removerTag("copiar", "#copia")
+        alert("Por favor, insira um texto para criptografar.");
+        return; // Retorna sem fazer mais nada se o input estiver vazio
+    }
+
+    // Verifica se o texto contém letras maiúsculas ou acentos
+    if (/[À-ÖØ-öø-ÿA-Z]/.test(input)) {
+        removerTag("copiar", "#copia")
+        restalrarTags()
+        alert("O texto não pode conter letras maiúsculas ou com acento.");
+        return; // Retorna sem fazer mais nada se o texto contiver letras maiúsculas ou com acento
+    }
+
+    if (!/[aeiou]/i.test(input)) {
+        restalrarTags()
+        removerTag("copiar", "#copia")
+        alert("é presciso de pelo menos uma vogal para ser criptografado")
+        return;
+    }
+
+
     let textarea = document.querySelector("#decodificador_texto");
     input = criptografar(input);
     textarea.value = input;
     removerTag("subtitulo", "#subtitulo");
     removerTag("imagem", "#img_garoto");
     organizar();
+    document.querySelector("#adicionar_texto").value = "";
 
     return input;
 }
@@ -31,34 +57,62 @@ function descriptografar(texto) {
     texto = texto.replace(/ai/g, "a");
     texto = texto.replace(/ober/g, "o");
     texto = texto.replace(/ufat/g, "u");
-    
+
     return texto;
 }
 
 function descriptografarTexto() {
     // chama a função descriptografar, e colocar o valor descriptografado no textarea.
     let input = document.querySelector("#adicionar_texto").value;
+
+    if (input === "") {
+        alert("Por favor, insira um texto criptografado para ser descriptografado.");
+        restalrarTags()
+    }
+
     let textarea = document.querySelector("#decodificador_texto");
     input = descriptografar(input);
     textarea.value = input
     removerTag("subtitulo", "#subtitulo")
     removerTag("imagem", "#img_garoto")
     organizar()
+    document.querySelector("#adicionar_texto").value = ""
     return input;
 }
 
-function removerTag (name,tag) {
+function removerTag(name, tag) {
     // retira as tags para ter uma melhor visão e colocara ou deixara as tags nescessarias.
     name = document.querySelector(tag);
     name.style.display = "none";
-    
+
 }
+
+function restalrarTags() {
+    if (window.innerWidth > 979) {
+        let img = document.querySelector("#img_garoto");
+        img.style.display = "flex"
+        let subtitulo = document.querySelector("#subtitulo")
+        subtitulo.style.display = "flex"
+    }
+    else {
+        removerTag("subtitulo", "#subtitulo")
+        removerTag("imagem", "#img_garoto")
+    }
+}
+// Adiciona um listener para o evento 'resize' que chama a função de ajuste de tags
+window.addEventListener('resize', restalrarTags);
+
+// Chama a função de ajuste de tags quando a página é carregada
+window.addEventListener('load', restalrarTags);
+
+
+
 function organizar() {
     let botao = document.querySelector("#copia")
     botao.style.display = "flex"
     let areaDecodificador = document.querySelector(".decodificador")
     areaDecodificador.style.justifyContent = "space-between"
-    
+
 }
 
 function copiaTexto() {
